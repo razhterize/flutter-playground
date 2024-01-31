622b72ac35c418d714132eb85d5c3b8ecbfc7d65
razhterize <arza.mahendra.31@gmail.com>
2024-01-31 12:29:44 +0700

    initial DAO

---
+++ lib/providers/struct/struct_interface.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_playground/providers/struct/struct.dart';
+
+class StructProvider {
+  late final StructDatabase database;
+  StructProvider() {
+    database = StructDatabase();
+  }
+}
