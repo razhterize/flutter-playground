0da1029c0e427d57a2b132d3def4e10f938410e3
razhterize <arza.mahendra.31>
2025-12-14 21:20:07 +0700

    feat(ui): stat name and stat value picker

---
+++ lib/ui/widgets/stat_picker.dart
@@ -0,0 +1,114 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/services.dart';
+import 'package:ww_optimizer/wuthering/stat.dart';
+
+class StatNamePicker extends StatefulWidget {
+  const StatNamePicker({
+    super.key,
+    required this.onChange,
+    this.enabled = true,
+  });
+
+  final bool enabled;
+  final void Function(StatName name) onChange;
+
+  @override
+  State<StatNamePicker> createState() => _StatNamePickerState();
+}
+
+class _StatNamePickerState extends State<StatNamePicker> {
+  StatName _statName = StatName.ATK;
+  @override
+  Widget build(BuildContext context) {
+    return ComboBox<StatName>(
+      value: _statName,
+      onChanged: (value) {
+        _statName = value!;
+        widget.onChange(value);
+        setState(() {});
+      },
+      items: StatName.statNames.entries.map((e) {
+        return ComboBoxItem(
+          value: e.key,
+          child: Text(e.value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class StatValuePicker extends StatefulWidget {
+  const StatValuePicker({
+    super.key,
+    required this.onChange,
+    required this.statValue,
+    this.buttonPress,
+    this.enable = true,
+  });
+
+  final StatValue statValue;
+  final void Function()? buttonPress;
+  final void Function(StatValue statValue) onChange;
+  final bool enable;
+
+  @override
+  State<StatValuePicker> createState() => _StatValuePickerState();
+}
+
+class _StatValuePickerState extends State<StatValuePicker> {
+  late StatValue _statValue;
+
+  @override
+  void initState() {
+    _statValue = widget.statValue;
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: .start,
+      crossAxisAlignment: .center,
+      children: [
+        SizedBox(
+          width: 40,
+          height: 32,
+          child: FilledButton(
+            onPressed: widget.enable ? widget.buttonPress : null,
+            style: ButtonStyle(),
+            child: Icon(FluentIcons.skype_minus),
+          ),
+        ),
+        Container(width: 5, color: Colors.transparent),
+        Flexible(
+          flex: 9,
+          child: StatNamePicker(onChange: nameFn, enabled: widget.enable),
+        ),
+        Container(width: 5, color: Colors.transparent),
+        Flexible(
+          flex: 10,
+          child: NumberBox<double>(
+            value: _statValue.value,
+            placeholder: "Value",
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r"[\d\.\,]")),
+            ],
+            keyboardType: .number,
+            onChanged: valueFn,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void nameFn(StatName name) {
+    _statValue = _statValue.copyWith(name: name);
+    widget.onChange(_statValue);
+  }
+
+  void valueFn(double? val) {
+    _statValue = _statValue.copyWith(value: val);
+    widget.onChange(_statValue);
+  }
+}
