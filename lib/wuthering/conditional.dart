b6082db9a8de326947dd15696e106ca469be49c5
razhterize <arza.mahendra.31>
2025-12-14 11:32:34 +0700

    feat(conditional): return null if predicate failed

--- lib/wuthering/conditional.dart
+++ lib/wuthering/conditional.dart
@@ -4,8 +4,7 @@ import 'package:ww_optimizer/wuthering/stat.dart';
 class Conditional<T> {
   late T _object;
   bool Function([Object?])? _activeOn;
-  T get object =>
-      _activeOn == null || _activeOn!() ? _object : throw Exception("Conditional not active");
+  T? get object => _activeOn == null || _activeOn!() ? _object : null;
   set object(T object) => _object = object;
   set activeOn(bool Function([Object?])? condition) => _activeOn = condition;
 }
