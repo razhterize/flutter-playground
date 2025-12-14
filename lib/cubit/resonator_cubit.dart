8f1094ea293c6a259f672e17e4ce7f816a9157c9
razhterize <arza.mahendra.31>
2025-12-14 21:19:06 +0700

    feat(cubit): update resonator

--- lib/cubit/resonator_cubit.dart
+++ lib/cubit/resonator_cubit.dart
@@ -25,12 +25,12 @@ class ResonatorCubit extends Cubit<ResonatorState> {
 
     // Listen for saved state
     _subs = savedCubit.stream.listen((savedState) {
-      emit(state.copyWith(true));
+      emit(state.copyWith(processing: true));
       final resonatorList = savedState.resonators
           .map((e) => Resonator.fromJson(e))
           .toList();
       resonatorList.sort(sortResonator);
-      emit(state.copyWith(false, resonatorList));
+      emit(state.copyWith(processing: false, resonators: resonatorList));
     });
 
     // Automagically save resonators
@@ -43,17 +43,31 @@ class ResonatorCubit extends Cubit<ResonatorState> {
         .map((e) => Resonator.fromJson(e))
         .toList();
     resonatorList.sort(sortResonator);
-    emit(state.copyWith(false, resonatorList));
+    emit(state.copyWith(processing: false, resonators: resonatorList));
+  }
+
+  void editResonator(Resonator resonator) {
+    emit(state.copyWith(editedResonator: resonator));
+  }
+
+  void updateResonator(Resonator resonator) {
+    emit(state.copyWith(processing: true));
+    final newList = state.resonators;
+    final index = newList.indexWhere((r) => r.name == resonator.name);
+    newList[index] = resonator;
+    emit(state.copyWith(processing: false, resonators: newList));
   }
 
   void addResonator(Resonator resonator) {
-    emit(state.copyWith(state.processing, [...state.resonators, resonator]));
+    var newList = state.resonators;
+    newList.add(resonator);
+    emit(state.copyWith(resonators: newList));
   }
 
   void removeResonator(Resonator resonator) {
-    emit(state.copyWith(true));
+    emit(state.copyWith(processing: true));
     state.resonators.remove(resonator);
-    emit(state.copyWith(false, state.resonators));
+    emit(state.copyWith(processing: false, resonators: state.resonators));
   }
 
   @override
@@ -75,13 +89,18 @@ class ResonatorState extends Equatable {
     this.editedResonator,
   ]);
 
-  ResonatorState copyWith([bool? processing, List<Resonator>? resonators]) {
+  ResonatorState copyWith({
+    bool? processing,
+    List<Resonator>? resonators,
+    Resonator? editedResonator,
+  }) {
     return ResonatorState(
       processing ?? this.processing,
       resonators ?? this.resonators,
+      editedResonator ?? this.editedResonator,
     );
   }
 
   @override
-  List<Object?> get props => [processing, resonators];
+  List<Object?> get props => [processing, resonators, editedResonator];
 }
