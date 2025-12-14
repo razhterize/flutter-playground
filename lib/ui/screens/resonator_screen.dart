22d7744b0940ac7296e87ddd3dd95d034755db8f
razhterize <arza.mahendra.31>
2025-12-14 21:20:37 +0700

    feat(ui): somewhat finished resonator editor.
    still need buffs and skills editor though

--- lib/ui/screens/resonator_screen.dart
+++ lib/ui/screens/resonator_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ww_optimizer/assets.dart';
+import 'package:ww_optimizer/core/types.dart';
+import 'package:ww_optimizer/paths.dart';
 import 'package:ww_optimizer/ui/widgets/images.dart';
+import 'package:ww_optimizer/ui/widgets/stat_picker.dart';
 import 'package:ww_optimizer/wuthering/resonator.dart';
 import 'package:ww_optimizer/wuthering/stat.dart';
 import 'package:ww_optimizer/cubit/resonator_cubit.dart';
@@ -15,20 +20,26 @@ class ResonatorScreen extends StatefulWidget {
 }
 
 class _ResonatorScreenState extends State<ResonatorScreen> {
-  Resonator? _editedResonator = null;
   final _filterController = TextEditingController();
   ElementType _filterElement = ElementType.None;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: .min,
-      crossAxisAlignment: .start,
-      mainAxisAlignment: .spaceEvenly,
-      children: [
-        Flexible(child: _resonatorFilter(context)),
-        Flexible(flex: 9, child: _resonatorGridView()),
-      ],
+    return ResonatorBuilder(
+      builder: (_, state) {
+        if (state.editedResonator != null) {
+          return ResonatorEditor();
+        }
+        return Column(
+          mainAxisSize: .min,
+          crossAxisAlignment: .start,
+          mainAxisAlignment: .spaceEvenly,
+          children: [
+            Flexible(child: _resonatorFilter(context)),
+            Flexible(flex: 9, child: _resonatorGridView()),
+          ],
+        );
+      },
     );
   }
 
@@ -80,10 +91,146 @@ class _ResonatorScreenState extends State<ResonatorScreen> {
           ),
           itemBuilder: (context, index) => Padding(
             padding: const EdgeInsets.all(20.0),
-            child: ResonatorImage(showedResonator[index]),
+            child: ResonatorImage(
+              showedResonator[index],
+              onClick: context.read<ResonatorCubit>().editResonator,
+            ),
           ),
         );
       },
     );
+  }
+}
+
+class ResonatorEditor extends StatefulWidget {
+  const ResonatorEditor({super.key});
+
+  @override
+  State<ResonatorEditor> createState() => _ResonatorEditorState();
+}
+
+class _ResonatorEditorState extends State<ResonatorEditor> {
+  late JsonType _rawJson;
+
+  @override
+  Widget build(BuildContext context) {
+    var edited = context.read<ResonatorCubit>().state.editedResonator!;
+    _rawJson = jsonDecode(
+      File(
+        "${assetDir.path}/resonators/${edited.name}.json",
+      ).readAsStringSync(),
+    );
+    return ResonatorBuilder(
+      builder: (_, state) {
+        return Column(
+          children: [
+            ListTile(
+              leading: ResonatorImage(edited, imageSize: Size(150, 150)),
+              title: TextBox(placeholder: edited.name, enabled: false),
+              subtitle: _levelSlider(context),
+            ),
+            _statsEditor(context),
+            _buffsEditor(context),
+            _skillsEditor(context),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _levelSlider(BuildContext context) {
+    var resonator = context.read<ResonatorCubit>().state.editedResonator!;
+    return Row(
+      children: [
+        const Text("Level", style: TextStyle(fontSize: 18)),
+        Flexible(
+          child: Slider(
+            min: 1,
+            max: 90,
+            label: "${resonator.level}",
+            value: resonator.level.toDouble(),
+            onChanged: (v) {
+              resonator.level = v.toInt();
+              // TODO: Take json value from _rawJson and put it as stats
+              StatMap _levelStats = {
+                StatName.ATK.value: _rawJson["stats"]["${v.toInt()}"]["ATK"],
+                StatName.HP.value: _rawJson["stats"]["${v.toInt()}"]["HP"],
+                StatName.DEF.value: _rawJson["stats"]["${v.toInt()}"]["DEF"],
+              };
+              for (var levelStat in _levelStats.entries) {
+                resonator.stats.update(
+                  levelStat.key,
+                  (value) => levelStat.value,
+                  ifAbsent: () => levelStat.value,
+                );
+              }
+              _update(context);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _statsEditor(BuildContext context) {
+    var resonator = context.read<ResonatorCubit>().state.editedResonator!;
+    var statEntries = resonator.stats.entries.toList();
+    return Expander(
+      header: const Text("Stats"),
+      content: SizedBox(
+        height: 200,
+        child: ListView.separated(
+          separatorBuilder: (_, _) {
+            return Container(height: 10, color: Colors.transparent);
+          },
+          itemCount: resonator.stats.isEmpty ? 1 : resonator.stats.length + 1,
+          itemBuilder: (_, index) {
+            if (resonator.stats.isEmpty || index >= resonator.stats.length) {
+              return FilledButton(
+                child: Icon(FluentIcons.add),
+                onPressed: () {
+                  List<StatName> validKeys = StatName.values
+                      .where((n) => !resonator.stats.containsKey(n))
+                      .toList();
+                  resonator.stats.update(
+                    validKeys.first.value,
+                    (v) => 0,
+                    ifAbsent: () => 0,
+                  );
+                  setState(() {});
+                },
+              );
+            }
+            final entry = statEntries[index];
+            return StatValuePicker(
+              statValue: StatValue(StatName(entry.key), entry.value),
+              buttonPress: () {
+                resonator.stats.remove(entry.key);
+                _update(context);
+              },
+              onChange: (stat) {
+                resonator.stats.update(stat.name.value, (v) => stat.value);
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buffsEditor(BuildContext context) {
+    var resonator = context.read<ResonatorCubit>().state.editedResonator;
+    return Expander(header: const Text("Buffs"), content: Placeholder());
+  }
+
+  Widget _skillsEditor(BuildContext context) {
+    var resonator = context.read<ResonatorCubit>().state.editedResonator;
+    return Expander(header: const Text("Skills"), content: Placeholder());
+  }
+
+  void _update(BuildContext context) {
+    final cubit = context.read<ResonatorCubit>();
+    cubit.updateResonator(cubit.state.editedResonator!);
+    setState(() {});
   }
 }
