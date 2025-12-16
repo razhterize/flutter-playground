2c0025e29db08fb4220bb403fd13f266fc6e6105
razhterize <arza.mahendra.31>
2025-12-16 11:53:42 +0700

    feat(ui): sligtly better buff editor

--- lib/ui/widgets/buff_editor.dart
+++ lib/ui/widgets/buff_editor.dart
@@ -3,8 +3,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:ww_optimizer/logger.dart';
 import 'package:ww_optimizer/ui/widgets/common.dart';
 import 'package:ww_optimizer/ui/widgets/stat_picker.dart';
-import 'package:ww_optimizer/wuthering/buff.dart';
-import 'package:ww_optimizer/wuthering/stat.dart';
+import 'package:ww_optimizer/wuthering.dart';
 
 class BuffPicker extends StatefulWidget {
   const BuffPicker({
@@ -63,34 +62,21 @@ class _BuffPickerState extends State<BuffPicker> {
   }
 
   Widget _buildStatList() {
+    final mq = MediaQuery.of(context);
     return SizedBox(
-      height: 60,
-      child: ListView.builder(
-        itemCount: _buff.stats.length + 1,
-        itemBuilder: (_, index) {
-          if (_buff.stats.isEmpty || index >= _buff.stats.length) {
-            return FilledButton(
-              child: Icon(FluentIcons.add),
-              onPressed: () {
-                _buff.stats.add(StatValue(.ATK, 0));
-                _update();
-              },
-            );
-          }
-          final stat = _buff.stats[index];
-          return Text(
-            "${StatName.strNames[stat.name]}:  ${stat.value}${stat.isPercent ? '%' : ''}",
-          );
-        },
+      height: mq.orientation == .landscape ? 20 : 40,
+      child: ListView(
+        children: _buff.stats.map((sv) => Text(sv.toString())).toList(),
       ),
     );
   }
 
+  // Buff Flyout. Where actual buff configuration happens
   void _openFlyout() async {
     _flyoutController.showFlyout<void>(
-      
+      placementMode: .topCenter,
       dismissOnPointerMoveAway: false,
-      dismissWithEsc: false,
+      dismissWithEsc: true,
       builder: (_) {
         return FlyoutContent(
           child: SizedBox(
@@ -106,6 +92,7 @@ class _BuffPickerState extends State<BuffPicker> {
                 ),
                 CommonUI.spacer(height: 10),
                 NumberBox(
+                  mode: .inline,
                   value: _buff.maxStack,
                   placeholder: "Max Stacks",
                   onChanged: _stackChange,
@@ -113,16 +100,26 @@ class _BuffPickerState extends State<BuffPicker> {
                 CommonUI.spacer(height: 10),
                 const Text("Stats"),
                 CommonUI.spacer(),
-                ..._buff.stats.map((e) {
+                ..._buff.stats.asMap().entries.map((entry) {
                   return StatValuePicker(
-                    onChange: (statValue) {
-                      final statIndex = _buff.stats.indexOf(statValue);
-                      _buff.stats[statIndex] = statValue;
+                    buttonPress: () {
+                      _buff.stats.removeAt(entry.key);
                       _update();
                     },
-                    statValue: e,
+                    onChange: (statValue) {
+                      _buff.stats[entry.key] = statValue;
+                      _update();
+                    },
+                    statValue: entry.value,
                   );
                 }),
+                FilledButton(
+                  child: Icon(FluentIcons.add),
+                  onPressed: () {
+                    _buff.stats.add(StatValue(.ATK, 0));
+                    _update();
+                  },
+                ),
               ],
             ),
           ),
