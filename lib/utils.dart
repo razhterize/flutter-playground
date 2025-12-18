3ffffdfd429e292fa1233320515fcc3ce39baa76
razhterize <arza.mahendra.31>
2025-12-18 15:11:30 +0700

    feat(utils): added function to measure execution time

---
+++ lib/utils.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'dart:async';
+import 'logger.dart';
+
+final Stopwatch _stopwatch = Stopwatch();
+
+void timeFunction(Function() func, [String? name]) {
+  if (_stopwatch.isRunning) {
+    _stopwatch.stop();
+    _stopwatch.reset();
+  }
+  _stopwatch.start();
+  func();
+  _stopwatch.stop();
+  final ms = _stopwatch.elapsedMilliseconds;
+  final us = _stopwatch.elapsedMicroseconds;
+  rootLogger.info("${name ?? func} execution takes $us μs / $ms ms");
+}
