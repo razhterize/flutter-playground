ea68d19fd0ed56b413dc89174610454f8c25201f
razhterize <arza.mahendra.31>
2025-12-20 20:00:49 +0700

    feat(core): update Pair to use final and copyWith

--- lib/core/types.dart
+++ lib/core/types.dart
@@ -1,7 +1,12 @@
+import 'package:ww_optimizer/wuthering/stat.dart';
+
 typedef JsonType = Map<String, dynamic>;
 
 class Pair<T1, T2> {
   final T1 first;
   final T2 second;
-  const Pair(this.first, this.second);
+  const Pair({required this.first, required this.second});
+  Pair<T1, T2> copyWith({T1? first, T2? second}) {
+    return Pair(first: first ?? this.first, second: second ?? this.second);
+  }
 }
