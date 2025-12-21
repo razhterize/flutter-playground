55ca7fed065056cb0ac46080272fc4855b93c262
razhterize <arza.mahendra.31>
2025-12-21 15:55:09 +0700

    feat: added method to get all JSON in a folder

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
+import '../core/types.dart';
+
 import 'logger.dart';
 
 final Stopwatch _stopwatch = Stopwatch();
@@ -15,6 +18,20 @@ void timeFunction(Function() func, [String? name]) {
   final ms = _stopwatch.elapsedMilliseconds;
   final us = _stopwatch.elapsedMicroseconds;
   rootLogger.info("${name ?? func} execution takes $us μs / $ms ms");
+}
+
+/// Parse all json in [Directory] dir
+/// Key will be file's name without extension
+JsonType getAllJson(Directory dir) {
+  JsonType results = {};
+  bool pred(File f) => f.path.endsWith('.json');
+  final files = dir.listSync().whereType<File>().where(pred);
+  for (var f in files) {
+    String key = f.path.split('/').last.replaceAll('.json', '');
+    JsonType json = jsonDecode(f.readAsStringSync());
+    results.addAll({key: json});
+  }
+  return results;
 }
 
 extension PrecisionRound on double {
