bd092cf335bfb039aed1fe1f1159202e6576d1fa
razhterize <arza.mahendra.31>
2025-12-22 16:30:00 +0700

    feat(core): add weapon level

--- lib/core/wuthering/weapon.dart
+++ lib/core/wuthering/weapon.dart
@@ -4,6 +4,7 @@ part of '../wuthering.dart';
 class Weapon {
   final int id;
   final String name;
+  final int level;
   final WeaponType type;
   final StatList stats;
   final BuffList buffs;
@@ -11,10 +12,30 @@ class Weapon {
   Weapon({
     this.id = 0,
     this.name = "",
+    this.level = 90,
     this.type = WeaponType.Sword,
     this.stats = const [],
     this.buffs = const [],
   });
+
+  Weapon copyWith({
+    int? id,
+    String? name,
+    int? level,
+    WeaponType? type,
+    StatList? stats,
+    BuffList? buffs,
+  }) {
+    return Weapon(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      level: level ?? this.level,
+      type: type ?? this.type,
+      stats: stats ?? this.stats,
+      buffs: buffs ?? this.buffs,
+    );
+  }
+
   factory Weapon.fromJson(JsonType json) => _$WeaponFromJson(json);
   JsonType toJson() => _$WeaponToJson(this);
 }
