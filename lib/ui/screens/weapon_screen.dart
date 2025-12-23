d28f7b52b62703fbb4f6b7c71a42caa698fe9f0a
razhterize <arza.mahendra.31>
2025-12-23 14:53:03 +0700

    feat(ui): at least usable weapon editor

--- lib/ui/screens/weapon_screen.dart
+++ lib/ui/screens/weapon_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mix/mix.dart';
 import 'package:ww_optimizer/ui/style.dart';
+import 'package:ww_optimizer/ui/widgets/buff_editor.dart';
+import 'package:ww_optimizer/ui/widgets/stat_picker.dart';
 
 import '../widgets/images.dart';
 import '../widgets/common.dart';
@@ -24,7 +26,7 @@ class _WeaponScreenState extends State<WeaponScreen> {
       child: VBox(
         style: vboxStyle,
         children: [
-          Box(style: cardStyle.add($box.maxHeight(600)), child: WeaponEditor()),
+          Box(style: cardStyle.add($box.maxHeight(400)), child: WeaponEditor()),
           Box(
             style: cardStyle.add($box.maxHeight(800)),
             child: _WeaponInventory(),
@@ -58,34 +60,155 @@ class _WeaponEditorState extends State<WeaponEditor> {
   @override
   Widget build(BuildContext context) {
     _cubit = context.read<WeaponCubit>();
+    return BlocListener<WeaponCubit, WeaponState>(
+      listener: (_, state) {
+        _weapon = state.editedWeapon!;
+        _nameController.text = _weapon.name;
+      },
+      child: VBox(
+        style: vboxStyle,
+        children: [
+          HBox(
+            style: hboxStyle,
+            children: [
+              FilledButton.icon(
+                onPressed: () => _cubit.editWeapon(_weapon),
+                label: Icon(Icons.add),
+              ),
+              FilledButton.icon(
+                onPressed: () => _cubit.addWeapon(_weapon),
+                label: Icon(Icons.save),
+              ),
+            ],
+          ),
+          Expanded(
+            child: WeaponBuilder(
+              builder: (_, state) {
+                if (state.editedWeapon == null) {
+                  return Center(
+                    child: Text(
+                      textAlign: .center,
+                      "No Edited Weapon.\n"
+                      "Create new or pick from inventory below",
+                    ),
+                  );
+                }
+                return VBox(
+                  style: vboxStyle,
+                  children: [
+                    _weaponInfo(),
+                    Expanded(
+                      child: HBox(
+                        style: hboxStyle,
+                        children: [
+                          Expanded(child: _weaponStats()),
+                          Expanded(child: _weaponBuffs()),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _weaponInfo() {
+    return HBox(
+      style: hboxStyle,
+      children: [
+        WeaponImage(_weapon, showName: false, imageSize: Size(100, 100)),
+        Expanded(
+          child: VBox(
+            style: vboxStyle,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: _labelDecor("Weapon Name"),
+              ),
+              // TODO: Level Slider
+              HBox(
+                style: hboxStyle,
+                children: [
+                  Text("Level: ${_weapon.level}"),
+                  Expanded(
+                    child: Slider(
+                      min: 0,
+                      max: 90,
+                      value: _weapon.level.toDouble(),
+                      onChanged: (v) {
+                        _updateWeapon(level: v.toInt());
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _weaponStats() {
     return VBox(
       style: vboxStyle,
       children: [
-        Expanded(
-          child: HBox(
-            style: hboxStyle,
-            children: [
-              WeaponBuilder(
-                builder: (_, state) {
-                  if (state.editedWeapon != null) {
-                    return WeaponImage(state.editedWeapon!);
-                  }
-                  return Placeholder();
-                },
-              ),
-              Expanded(
-                child: VBox(
-                  style: vboxStyle,
-                  children: [
-                    TextField(
-                      controller: _nameController,
-                      decoration: _labelDecor("Weapon Name"),
-                    ),
-                    // TODO: Level Slider
-                  ],
-                ),
-              ),
-            ],
+        ..._weapon.stats.map((sv) {
+          return StatValuePicker(
+            statValue: sv,
+            buttonPress: () {
+              _weapon.stats.remove(sv);
+              _updateWeapon(stats: _weapon.stats);
+            },
+            onChange: (newStats) {
+              var _stats = _weapon.stats;
+              _stats[_stats.indexOf(sv)] = newStats;
+              _updateWeapon(stats: _stats);
+            },
+          );
+        }),
+        Tooltip(
+          message: "Add Stat",
+          child: FilledButton(
+            onPressed: () {
+              _updateWeapon(stats: [..._weapon.stats, StatValue()]);
+            },
+            child: Icon(Icons.add),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _weaponBuffs() {
+    return VBox(
+      style: vboxStyle,
+      children: [
+        ..._weapon.buffs.map((buff) {
+          return BuffPicker(
+            buff: buff,
+            buttonPress: () {
+              _weapon.buffs.remove(buff);
+              _updateWeapon(buffs: _weapon.buffs);
+            },
+            onChange: (newBuff) {
+              var _buffs = _weapon.buffs;
+              _buffs[_buffs.indexOf(buff)] = newBuff;
+              _updateWeapon(buffs: _buffs);
+            },
+          );
+        }),
+        Tooltip(
+          message: "Add Buff",
+          child: FilledButton(
+            onPressed: () {
+              _updateWeapon(buffs: [..._weapon.buffs, Buff()]);
+            },
+            child: Icon(Icons.add),
           ),
         ),
       ],
@@ -109,6 +232,7 @@ class _WeaponEditorState extends State<WeaponEditor> {
       buffs: buffs,
     );
     _cubit.editWeapon(_weapon);
+    setState(() {});
   }
 
   InputDecoration _labelDecor(String text) =>
@@ -169,22 +293,25 @@ class _WeaponInventoryState extends State<_WeaponInventory> {
 
   Widget _weaponFilter(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return CommonUI.padding8(
-      child: SizedBox(
-        height: 30,
-        width: size.width,
-        child: Row(
-          children: [
-            // Name search box
-            Flexible(
-              child: TextField(
-                controller: _filterController,
-                onChanged: (_) => setState(() {}),
-                expands: false,
+    return SizedBox(
+      height: 40,
+      width: size.width,
+      child: Row(
+        children: [
+          // Name search box
+          Flexible(
+            child: TextField(
+              controller: _filterController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                isDense: true,
+                label: Text("Search Weapon"),
               ),
+              onChanged: (_) => setState(() {}),
+              expands: false,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
